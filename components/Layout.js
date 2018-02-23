a66b60399654d05a6c2ab161f786301514286a34
pllearns <pllearns@gmail.com>
2018-02-22 16:13:40 -0800

    Update segment responsiveness, still receive getBoundingClientRect err

--- components/Layout.js
+++ components/Layout.js
@@ -9,52 +9,51 @@ import Footer from './Footer'
 export default props => {
   return (
     <Segment>
-
-      <Segment.Group>
-        <Responsive as={Segment} {...Responsive.onlyMobile}>
-          <Container>
-            <Head>
-              <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-            </Head>
+      <Container>
+        <Head>
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+        </Head>
+        <Segment.Group>
+          <Responsive {...Responsive.onlyMobile}>
             <Header>
               {props.children}
             </Header>
             <HomeMainTitle mobile />
             <HomeCCAbout mobile />
             <Footer />
-          </Container>
-        </Responsive>
-      </Segment.Group>
-      <Segment.Group>
-        <Responsive as={Segment} {...Responsive.onlyTablet}>
-          <Container>
-            <Head>
-              <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-            </Head>
+          </Responsive>
+        </Segment.Group>
+      </Container>
+      <Container>
+        <Head>
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+        </Head>
+        <Segment.Group>
+          <Responsive {...Responsive.onlyTablet}>
             <Header>
               {props.children}
             </Header>
             <HomeMainTitle mobile />
             <HomeCCAbout mobile />
             <Footer />
-          </Container>
-        </Responsive>
-      </Segment.Group>
-      <Segment.Group>
-        <Responsive as={Segment} {...Responsive.onlyComputer}>
-          <Container>
-            <Head>
-              <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-            </Head>
+          </Responsive>
+        </Segment.Group>
+      </Container>
+      <Container>
+        <Head>
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+        </Head>
+        <Segment.Group>
+          <Responsive {...Responsive.onlyComputer}>
             <Header>
               {props.children}
             </Header>
             <HomeMainTitle mobile />
             <HomeCCAbout mobile />
             <Footer />
-          </Container>
-        </Responsive>
-      </Segment.Group>
+          </Responsive>
+        </Segment.Group>
+      </Container>
     </Segment>
   )
 }
