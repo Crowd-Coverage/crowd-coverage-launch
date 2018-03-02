02882bc8b6042b72d719d634b43f1d2eb86f23c9
pllearns <pllearns@gmail.com>
2018-03-02 15:31:40 -0800

    start structs

--- ethereum/contracts/CrowdCoverageContract.sol
+++ ethereum/contracts/CrowdCoverageContract.sol
@@ -1,5 +1,8 @@
 pragma solidity ^0.4.17;
 
 contract CrowdCoverageContract {
-  
+  struct Insured {
+    mapping(address => uint) user;
+    mapping(uint => uint) covrBalance;
+  }
 }
--- ethereum/contracts/PolicyStart.sol
+++ ethereum/contracts/PolicyStart.sol
@@ -3,5 +3,14 @@ pragma solidity ^0.4.17;
 import "./CrowdCoverageContract.sol";
 
 contract PolicyStart is CrowdCoverageContract {
-
+  // think about policy bidding and how that translates to policyProvider. 
+  struct Policy {
+    uint policyId;
+    uint productId;
+    string productName;
+    uint productValue;
+    uint requestedMaxClaim;
+    mapping(address => uint) policyUser;
+    mapping(address => uint) policyProvider;
+  }
 }
