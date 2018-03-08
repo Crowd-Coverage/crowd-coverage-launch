af8086aab5dd0ffcbe6e66f45581df1f273d0471
pllearns <pllearns@gmail.com>
2018-03-08 13:26:51 -0800

    add analytics

--- components/Head.js
+++ components/Head.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import ReactGA from 'react-ga'
 import Head from 'next/head'
+import Router from 'next/router'
+
+const gaTrackingId = 'UA-115469214-1'
+
+Router.onRouteChangeComplete = () => {
+  ReactGA.initialize(gaTrackingId)
+  ReactGA.pageview(window.location.pathname)
+}
 
 export default () => {
   return (
     <Head>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`} />
+      <script dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || []
+          function gtag(){
+            dataLayer.push(arguments)
+          }
+          gtag('js', new Date())
+          gtag('config', '${gaTrackingId}')
+        `}} />
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
